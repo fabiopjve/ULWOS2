2edef346d4b370ed8191cdb59fdad5ac39b3da31
Fábio Pereira <fabio.jve@gmail.com>
2020-05-19 19:22:57 -0400

    Improved git ignore

--- examples/Linux/ULWOS2_demo1.c
+++ examples/Linux/ULWOS2_demo1.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-ULWOS2 example with two threads
+ULWOS2 example with four threads
 Author: Fábio Pereira
 Date: Apr, 23, 2020
 embeddedsystems.io
@@ -12,86 +12,117 @@ embeddedsystems.io
 #define SIGNAL_RUN_THREAD3  1
 #define SIGNAL_RUN_THREAD4  2
 
+#define CLEAR_TERMINAL() printf("\x1B[2J")
+#define PRINT_RED() printf("\x1B[31m")
+#define PRINT_GREEN() printf("\x1B[32m")
+#define PRINT_BLUE() printf("\x1B[34m")
+#define PRINT_WHITE() printf("\x1B[0m")
+
+void asciiCursorXY(uint8_t x, uint8_t y)
+{
+  printf("\x1B[%hu;%huH",y,x);
+}
+
+void printCharXY(uint8_t x, uint8_t y, char ch)
+{
+  asciiCursorXY(x,y);
+  printf("%c",ch);
+  fflush(stdout);
+}
+
 void testThread1(void)
 {
-    static uint8_t counter = 5;
+    static uint8_t posX = 0;
+    const uint8_t posY = 2;
+    PRINT_RED();
     ULWOS2_THREAD_START();
+    asciiCursorXY(0,posY);
+    printf("Thread 1:");
     while (1) {
-        printf(".");
-        fflush(stdout);
-        ULWOS2_THREAD_YIELD();
-        printf(".");
-        fflush(stdout);
-        ULWOS2_THREAD_SLEEP_MS(250);
-        printf("1");
-        fflush(stdout);
-        ULWOS2_THREAD_YIELD();
-        printf("1");
-        fflush(stdout);
-        ULWOS2_THREAD_SLEEP_MS(250);
-        counter--;
-        if (counter == 0) {
-            counter = 5;
-            ULWOS2_THREAD_SLEEP_MS(3000);
-            ULWOS2_THREAD_SEND_SIGNAL(SIGNAL_RUN_THREAD3);
-        }
+        if (posX<20) {
+            printCharXY(10+posX,posY,'.');
+            ULWOS2_THREAD_SLEEP_MS(200);
+            if (posX == 10) ULWOS2_THREAD_SEND_SIGNAL(SIGNAL_RUN_THREAD3);
+            posX++;
+        } else break;
     }
+    asciiCursorXY(10+posX,posY);
+    printf(" Done!\n");
+    ULWOS2_THREAD_KILL();
 }
 
 void testThread2(void)
 {
+    static uint8_t posX = 0;
+    const uint8_t posY = 3;
+    PRINT_BLUE();
     ULWOS2_THREAD_START();
+    asciiCursorXY(0,posY);
+    printf("Thread 2:");
     while (1) {
-        printf("!");
-        fflush(stdout);
-        ULWOS2_THREAD_YIELD();
-        printf("!");
-        fflush(stdout);
-        ULWOS2_THREAD_SLEEP_MS(250);
-        printf("2");
-        fflush(stdout);
-        ULWOS2_THREAD_YIELD();
-        printf("2");
-        fflush(stdout);
-        ULWOS2_THREAD_SLEEP_MS(260);
+        if (posX<20) {
+            printCharXY(10+posX,posY,'.');
+            ULWOS2_THREAD_SLEEP_MS(300);
+            posX++;
+        } else break;
     }
+    asciiCursorXY(10+posX,posY);
+    printf(" Done!\n");
+    ULWOS2_THREAD_SEND_SIGNAL(SIGNAL_RUN_THREAD4);
+    ULWOS2_THREAD_KILL();
 }
 
 void testThread3(void)
 {
+    static uint8_t posX = 0;
+    const uint8_t posY = 4;
+    PRINT_GREEN();
     ULWOS2_THREAD_START();
-    while(1) {
-        printf("T3 WAIT 1 ");
-        fflush(stdout);
-        ULWOS2_THREAD_WAIT_FOR_SIGNAL(SIGNAL_RUN_THREAD3);
-        ULWOS2_THREAD_SEND_SIGNAL(SIGNAL_RUN_THREAD4);
+    asciiCursorXY(0,posY);
+    printf("Thread 3:");
+    ULWOS2_THREAD_WAIT_FOR_SIGNAL(SIGNAL_RUN_THREAD3);
+    while (1) {
+        if (posX<20) {
+            printCharXY(10+posX,posY,'.');
+            ULWOS2_THREAD_SLEEP_MS(250);
+            posX++;
+        } else break;
     }
+    asciiCursorXY(10+posX,posY);
+    printf(" Done!\n");
+    ULWOS2_THREAD_KILL();
 }
 
 void testThread4(void)
 {
-    static uint8_t counter = 5;
+    static uint8_t posX = 0;
+    const uint8_t posY = 5;
+    PRINT_WHITE();
     ULWOS2_THREAD_START();
-    while(1) {
-        printf("T4 WAIT 3 ");
-        fflush(stdout);
-        ULWOS2_THREAD_WAIT_FOR_SIGNAL(SIGNAL_RUN_THREAD4);
-        counter--;
-        if (counter == 0) {
-            printf("T4 BYE ");
-            fflush(stdout);
-            ULWOS2_THREAD_KILL();
-        }
+    asciiCursorXY(0,posY);
+    printf("Thread 4:");
+    ULWOS2_THREAD_WAIT_FOR_SIGNAL(SIGNAL_RUN_THREAD4);
+    while (1) {
+        if (posX<20) {
+            printCharXY(10+posX,posY,'.');
+            ULWOS2_THREAD_SLEEP_MS(250);
+            posX++;
+        } else break;
     }
+    asciiCursorXY(10+posX,posY);
+    printf(" Done!\n");
+    ULWOS2_THREAD_KILL();
 }
 
 int main()
 {
+    CLEAR_TERMINAL();
+    asciiCursorXY(0,1);
     printf("ULWOS2 demo!\n");
     ULWOS2_INIT();
     ULWOS2_THREAD_CREATE(testThread1, 1);
     ULWOS2_THREAD_CREATE(testThread2, 2);
-    ULWOS2_THREAD_CREATE(testThread3, 20);
-    ULWOS2_THREAD_CREATE(testThread4, 20);
+    ULWOS2_THREAD_CREATE(testThread3, 3);
+    ULWOS2_THREAD_CREATE(testThread4, 4);    
     ULWOS2_START_SCHEDULER();
 }
