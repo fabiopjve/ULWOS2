20ab0a0c70dc7843b9d247fa6710284894d7f162
Fábio Pereira <fabio.jve@gmail.com>
2020-09-07 09:06:25 -0400

    Updated LED blinker comments

--- examples/RISC-V/LEDblinker_GD32V/src/main.c
+++ examples/RISC-V/LEDblinker_GD32V/src/main.c
@@ -9,7 +9,10 @@
 #include "gd32v_pjt_include.h"
 #include "ULWOS2.h"
 
-void thread1(void)
+/*
+    Blinks red LED (50ms on and 450ms off)
+*/
+void ledBlinker_thread(void)
 {
     ULWOS2_THREAD_START();
     while(1)
@@ -17,7 +20,7 @@ void thread1(void)
         LEDR(0);    // red LED on
         ULWOS2_THREAD_SLEEP_MS(50);
         LEDR(1);    // red LED off
-        ULWOS2_THREAD_SLEEP_MS(500);
+        ULWOS2_THREAD_SLEEP_MS(450);
     }
 }
 
@@ -28,15 +31,15 @@ void longanNanoInit(void)
     rcu_periph_clock_enable(RCU_GPIOC);
     gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_13);
     gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_1|GPIO_PIN_2);
-    LEDR(1);
-    LEDG(1);
-    LEDB(1);    
+    LEDR(1);    // red LED off
+    LEDG(1);    // green LED off
+    LEDB(1);    // blue LED off
 }
 
 void main(void)
 {
     longanNanoInit();
     ULWOS2_INIT();
-    ULWOS2_THREAD_CREATE(thread1, 10);
+    ULWOS2_THREAD_CREATE(ledBlinker_thread, 10);
     ULWOS2_START_SCHEDULER();	
 }
